34d71e25ec55a69d7fd2f139d1b6722f5a81de81
omiunu <you@example.com>
2023-02-22 14:09:35 -0800

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,11 +7,11 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(void)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-	} else if (n == 0)
 		return (1);
+	} else if (n == 0)
 }
